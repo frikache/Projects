5af4abbf4786d52d7fbb6b967f7510f59e4e6c9e
frikache <novikov_a@rambler.ru>
2014-01-21 15:35:40 +0300

    Проект Todo
    С выполненным удалением элементов

---
+++ WpfApplication3/WpfApplication3/Todo.cs
@@ -0,0 +1,17 @@
+﻿namespace TodoProject
+{
+    public class Todo
+    {
+        public int Number;
+        public string TodoStr;
+        public bool Done;
+
+        public Todo(int num, string el)
+        {
+            Number = num;
+            TodoStr = el;
+            Done = false;
+        }
+
+    }
+}
